b7f7b37e692516ca44f1b3434503c88ab46b7501
DarshanDeore01 <darshan.deore01@gmail.com>
2024-03-12 09:55:21 +0530

    New Mod Added

--- Pattern_Practice_programs_Loops_If_Else/HOTEL_MENU_CARD.c
+++ Pattern_Practice_programs_Loops_If_Else/HOTEL_MENU_CARD.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 
-int main() {
-    int choice,qty;
+int main()
+{
+    int choice, qty;
     float total = 0;
 
-    do {
+    do
+    {
         printf("\n");
         printf("Welcome to Hotel Menu\n");
         printf("1. Burger - Rs.50.00\n");
@@ -15,32 +17,33 @@ int main() {
         printf("Enter your choice: ");
         scanf("%d", &choice);
 
-        switch(choice) {
-            case 1:
-                printf("\n Enter the quantity to order:\n");
-                scanf("%d",&qty);
-                total = total + 50.00*qty;
-                printf("Burger added to your order.\n");
-                break;
-            case 2:
-                total = total + 80.00*qty;
-                printf("Pizza added to your order.\n");
-                break;
-            case 3:
-                total = total + 60.00*qty;
-                printf("Pasta added to your order.\n");
-                break;
-            case 4:
-                total = total + 100.00*qty;
-                printf("Sandwich added to your order.\n");
-                break;
-            case 5:
-                printf("Thank you for your order!\n");
-                break;
-            default:
-                printf("Invalid choice! Please enter a number between 1 and 5.\n");
+        switch (choice)
+        {
+        case 1:
+            printf("\n Enter the quantity to order:\n");
+            scanf("%d", &qty);
+            total = total + 50.00 * qty;
+            printf("Burger added to your order.\n");
+            break;
+        case 2:
+            total = total + 80.00 * qty;
+            printf("Pizza added to your order.\n");
+            break;
+        case 3:
+            total = total + 60.00 * qty;
+            printf("Pasta added to your order.\n");
+            break;
+        case 4:
+            total = total + 100.00 * qty;
+            printf("Sandwich added to your order.\n");
+            break;
+        case 5:
+            printf("Thank you for your order!\n");
+            break;
+        default:
+            printf("Invalid choice! Please enter a number between 1 and 5.\n");
         }
-    } while(choice != 5);
+    } while (choice != 5);
 
     printf("Your total bill is: Rs.%.2f\n", total);
 
